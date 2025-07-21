f242748a97496027e8b8ecda0195f694a9c46d37
Benton <bentonturner3@gmail.com>
2025-07-21 17:26:46 -0400

    update auth

--- task-api-service/middleware/auth.js
+++ task-api-service/middleware/auth.js
@@ -1,7 +1,3 @@
-import { Magic } from '@magic-sdk/admin';
-
-const magic = new Magic(process.env.MAGIC_SECRET_KEY);
-
 // Magic.link authentication middleware
 export default async function auth(req, res, next) {
   try {
